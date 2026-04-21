dfda514654da52019196bd569a2fa2cfa8dd6538
MsKul <50674098+zaxar16677@users.noreply.github.com>
2026-04-21 11:04:29 +0300

    Add files via upload

---
+++ homepage/javascript.js
@@ -0,0 +1,8 @@
+        function showMessage() {
+            document.getElementById("topMessage").classList.add("show");
+        }
+
+        // Функция скрытия сообщения
+        function hideMessage() {
+            document.getElementById("topMessage").classList.remove("show");
+        }
